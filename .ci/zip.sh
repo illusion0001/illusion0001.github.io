faea7b33c0cdd3ca6b72fd85d525623cfdfa6ac5
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-09-09 12:05:38 -0500

    Use version from patch repository

--- .ci/zip.sh
+++ .ci/zip.sh
@@ -1,11 +1,10 @@
-current=$(date '+%Y-%m-%d %H:%M:%S %A (%Z %z)')
-msg="Patch archive built on: \`$current\`"
 patch_file=patch_built.md
 cp -r _patch0 patch0
 ls -R _patch0 > manifest_patch0.txt
 find _patch0 -print > manifest_patch1.txt
-echo "$msg" >> patch0/$patch_file
-echo "$msg" >> _pages/$patch_file
+wget https://raw.githubusercontent.com/illusion0001/console-game-patches/gh-pages/$patch_file
+cat $patch_file > patch0/$patch_file
+cat $patch_file > _pages/$patch_file
 cat patch0/$patch_file
 zip -r _patch/patch.zip patch0
 rm  -r patch0
