0bb32dfb6956743b7f8a30711ea158eb70245ee0
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-11-16 13:13:00 -0600

    Update links to GoldHEN

--- .ci/zip.sh
+++ .ci/zip.sh
@@ -2,7 +2,7 @@ patch_file=patch_built.md
 cp -r _patch0 patch0
 ls -R _patch0 > manifest_patch0.txt
 find _patch0 -print > manifest_patch1.txt
-wget https://raw.githubusercontent.com/illusion0001/console-game-patches/gh-pages/$patch_file
+curl https://raw.githubusercontent.com/illusion0001/console-game-patches/gh-pages/$patch_file
 cat $patch_file > patch0/$patch_file
 cat $patch_file > _pages/$patch_file
 cat patch0/$patch_file
